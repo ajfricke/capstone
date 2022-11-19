5df9d3e38c43d1e4cb0bc3c679e8e4a9796cdeb7
Alex Fricke <ajfricke@edu.uwaterloo.ca>
2022-11-18 21:26:56 -0500

    Update LED control code

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -2,14 +2,19 @@
 int input_bit = -1;
 char input_byte = 'z';
 
-// example foot mapping
-array footsize_LED_mapping = {{1,1,1}, {2,2,2}, {3,3,3}, {4,4,4}};
+// footsize to LED positions mapping
+int footsize_LED_map_F[][2] = {{1,1}, {2,2}, {3,3}, {4,4}};
+int footsize_LED_map_M[][2] = {{1,1}, {2,2}, {3,3}, {4,4}};
+
+// LED position to LED pin mapping
+int LED_pos_to_pin_middle[] = {13, 14, 15, 16, 17};
+int LED_pos_to_pin_L[] = {13, 14, 15, 16, 17};
+int LED_pos_to_pin_R[] = {13, 14, 15, 16, 17};
 
 struct LED_pos {
-  int left_pos;
-  int right_pos;
+  int side_pos;
   int top_pos;
-  
+  int foot;
 };
 
 void setup() {
@@ -42,8 +47,7 @@ void main() {
     }
 
     // footsize (int), foot type (L,R), or LED positions (ie. 111)
-
-    
+  }
 }
 
 // LED SETUP FUNCTIONS
@@ -58,70 +62,78 @@ void start_setup() {
     } else {
       pos = footsize_to_LED_pos(input_byte, true);
       initialize_LEDs(pos);
+      handle_LEDs(pos);
       return;
     }
   }
 }
 
-struct LED_pos footsize_to_LED_pos(double footsize) {
+struct LED_pos footsize_to_LED_pos(int sex, int foot, double footsize) {
   struct LED_pos pos;
 
   // convert footsize to an index for the mapping
-  footsize_idx = foot_size/2*3;
-  mapping = footsize_LED_mapping[footsize_LED_mapping];
+  footsize_idx = foot_size/2*3; // just an example
+
+  // get LED locations based on footsize and sex
+  if (sex == 0) {
+    LED_locations = footsize_LED_map_F[footsize_LED_mapping];
+  } else {
+    LED_locations = footsize_LED_map_M[footsize_LED_mapping];
+  }
 
   // use mapping to link footsize to LED positions
-  pos.left_pos = mapping[0];
-  pos.right_pos = mapping[1];
-  pos.top_pos = mapping[2];
+  pos.side_pos = mapping[0];
+  pos.top_pos = mapping[1];
+  pos.foot = foot;
 
   return pos;
 }
 
-void initialize_LEDs(pos, move_LEDs_bool) {
-  // turn on LEDs
-
-  if (move_LEDs_bool == true) {
-    // solid red (unnoticable PWM)
-    // call handle_LEDs
-    handle_LEDs(pos);
-    return;
+// turn on LEDs
+void initialize_LEDs(struct pos) {
+  digitalWrite(LED_pos_to_pin_middle[pos.side_pos], HIGH)
+  
+  if (pos.foot == 0) {
+    digitalWrite(LED_pos_to_pin_R[pos.top_pos], HIGH);
   } else {
-    // solid green
-    return;
+    digitalWrite(LED_pos_to_pin_L[pos.top_pos], HIGH);
   }
 }
 
-void handle_LEDs(pos) {
-  // control set up
-  // in future, flash LEDs rather than putting them high
-  // need mapping of LED positions to pin output?
-  move_LED(pos.left_pos);
-  move_LED(pos.right_pos);
-  move_LED(pos.top_pos);
-  
-  return;
+// parent function to move control the movement of the LEDs
+void handle_LEDs(struct pos) {
+  move_LEDs(pos.side, LED_pos_to_pin_middle);
+
+  if (pos.foot == 0) {
+    move_LEDs(pos.top_pos, LED_pos_to_pin_R);
+  } else {
+    move_LEDs(pos.top_pos, LED_pos_to_pin_L);
+  }
 }
 
-void move_LED(pos) {
+
+// TODO: Add checks for invalid positions (i.e. goes too high)
+void move_LED(struct pos, int map[]) {
   // get information over BT
   while(Serial.available()>0) {
     input_bit = Serial.read()
     Serial.println(input_bit);
 
+    // move inwards
     if (input_bit) == 0 {
-      // move inwards
-      digitalWrite(pos, LOW);
-      digitalWrite(pos-1, HIGH);
+      digitalWrite(map[pos], LOW);
+      digitalWrite(map[pos-1], HIGH);
       pos = pos-1;
-    } else if (input_bit) == 1 {
-      // move outwards
-      digitalWrite(pos, LOW);
-      digitalWrite(pos+1, HIGH);
+    } 
+    // move outwards
+    else if (input_bit) == 1 {
+      digitalWrite(map[pos], LOW);
+      digitalWrite(map[pos+1], HIGH);
       pos = pos+1;
-    } else (input_bit) == 2 {
-      // send position to tablet over BT
-      return // LED placement confirmed
+    }
+    // send position to tablet over BT, LED placement confirmed
+    else (input_bit) == 2 {
+      return
     }
   }
 }
