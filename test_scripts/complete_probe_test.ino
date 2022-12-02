0a94fac4d98006a2d1285827217bc18ec84c8035
Alex Fricke <ajfricke@edu.uwaterloo.ca>
2022-12-02 16:46:28 -0500

    Update entire probe test script

--- test_scripts/complete_probe_test.ino
+++ test_scripts/complete_probe_test.ino
@@ -17,9 +17,15 @@ Servo myServo;
 #define microstep 1/4 // change if needed
 #define actuatorShaftLengthMM 50
 
+// for actuator probing
+// these settings will cause it to take 2.5 seconds to raise monofilament
+#define strokeStepMM 2
+#define delayMS 100
+
 // TODO: hardcoded now, will be determined by app later
 #define sex 1 // 0 for men, 1 for women
 #define footsize 10 
+int foot = 0; // 0 for right, 1 for left
 
 // 1st metatarsal (x,y) coordinates for different foot sizes
 // TODO: remove W5 (first) from this list, not needed
@@ -31,6 +37,8 @@ float m1CoordList[10][2] = {{17.86979,158.3}, {18.52799,163.83845}, {19.41122,17
 float m3Conversions[2] = {2.666243932,1.004125611};
 float m5Conversions[2] = {4.166072072,0.962256948};
 float bigToeConversions[2] = {1,1.231986944};
+
+float newReferencePoint[2] = {55, 181.54159};
 
 float w5FootsizeCoords[4][2] = {{17.86979, 158.3}, {47.63918, 157.93545}, {74.43552, 151.35002}, {17.86979, 193.75}};
 
@@ -76,9 +84,6 @@ void moveActuator(float strokeMM) {
 }
 
 bool raiseMonofilament(int strokeStepMM, int delayMS) {
-  int strokeStepMM = 2;
-  int delayMS = 500; // TODO: move
-
   // move actuator up to at most 50 mm, checking for laser cut from optocoupler every strokeStepMM
   for (int mmLocation = 1; mmLocation < actuatorShaftLengthMM; mmLocation += strokeStepMM) {
     int optoVal = analogRead(optocouplerPin); // read the value from the optocoupler
@@ -93,6 +98,13 @@ bool raiseMonofilament(int strokeStepMM, int delayMS) {
 
   moveActuator(0); // reset if laser not cut
   return false;
+}
+
+// move motors and actuator back to starting positions and exit
+void resetAndExit() {
+  moveXYRails(0, 0);
+  moveActuator(0);
+  exit(0);
 }
 
 void setup() {
@@ -124,9 +136,16 @@ void setup() {
   
   probingCoords = {m1Coords, m3Coords, m5Coords, bigToeCoords};
 
+  // recalculate probing coordinates with new reference point
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 2; j++) {
+        probingCoord[i][j] = probingCoord[i][j] - newReferencePoint[j];
+    }
+  }
+
   // shuffle the sequence of probing locations
   int j;
-  int t;
+  float temp[2];
   for (int i = 0; i < 4; i++) {
       j = random(0, n - i); // get random index
 
@@ -138,21 +157,54 @@ void setup() {
 }
 
 void loop() {
+  Serial.println('Starting test.')
   // move rails to each of the 4 probing locations
   for (int i = 0; i < 4; i++) {
-    moveXYRails(probingCoords[i][0], probingCoords[i][1]);
+    Serial.println('Moving XY rails to probing location.');
 
-    // test probing location three times
-    for (int j = 0; j < 3; j++) {
-        raiseMonofilament();
-        delay(1000); // TODO: randomize delay
+    // negate x-coordinate if left foot
+    if (foot == 1) {
+      moveXYRails(-probingCoords[i][0], probingCoords[i][1]);
+    } else {
+      moveXYRails(probingCoords[i][0], probingCoords[i][1]);
     }
 
-    delay(3000); // TODO: randomize delay
+    // test probing location three times
+    int improperProbeCount = 0;
+    for (int j = 0; j < 3; j++) {
+        Serial.println('Raising monofilament.');
+        properProbe = raiseMonofilament();
+
+        if (properProbe == false) {
+            j -= 1;
+            improperProbeCount += 1;
+        } else {
+            delay(2000); // hold probe to foot for 2 seconds
+        }
+
+        if (improperProbeCount == 3){
+            Serial.println('Monofilament is not exerting 10g force as determined by optocoupler after 3 tries. Exiting.');
+            resetAndExit();
+        }
+
+        Serial.println('Raising lowering.');
+        moveActuator(0); // lower monofilament back down
+
+        if (j != 2) {
+            delay(random(1000, 3000)); // randomizes same location probing at intervals between 1-3s
+        }
+    }
+
+    delay(random(1000, 3000)); // randomizes different location probing at intervals between 1-3s
   }
 
-  // move motors and actuator back to starting positions and exit
-  moveXYRails(0, 0);
-  moveActuator(0);
-  exit(0);
+  if (foot == 1) {
+    Serial.println('Test finished. Exiting.');
+    resetAndExit();
+  } else {
+    foot = 1; // test left foot next
+    moveXYRails(0, 0);
+    moveActuator(0);
+    Serial.println('Repeating test with left foot.');
+  }
 }
