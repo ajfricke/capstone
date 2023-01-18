2dda86a7360572f79fb7cd1d747e9fb938ac1fad
Alex Fricke <ajfricke@edu.uwaterloo.ca>
2023-01-17 23:46:20 -0500

    Update setup script to reflect hardware changes

--- test_scripts/LED_setup_test.ino
+++ test_scripts/LED_setup_test.ino
@@ -11,6 +11,7 @@ int buttonStateRail;
 
 int i = 0;
 int j;
+int maxPos;
 
 int leftState = 0;
 int rightState = 0;
@@ -19,18 +20,20 @@ String currState = "left";
 String prevState;
 int prevPos;
 
-int leftLEDs[12][4] = {{0,0,0,1}, {0,0,0,2}, {0,0,0,4}, {0,0,0,8},
-                    {0,0,0,16}, {0,0,0,32}, {0,0,0,64}, {0,0,0,128},
-                    {0,0,1,0}, {0,0,2,0}, {0,0,4,0}, {0,0,8,0}};
+int leftLEDs[10][4] = {{0,0,0,1}, {0,0,0,2}, {0,0,0,4}, {0,0,0,8},
+					{0,0,0,16}, {0,0,0,32}, {0,0,0,64}, {0,0,0,128},
+                  	{0,0,1,0}, {0,0,2,0}};
 
-int rightLEDs[12][4] = {{0,0,16,0}, {0,0,32,0}, {0,0,64,0}, {0,0,128,0},
-                    {0,1,0,0}, {0,2,0,32}, {0,4,0,0}, {0,8,0,0},
-                    {0,16,0,0}, {0,32,0,0}, {0,64,0,0}, {0,128,0,0}};
+int rightLEDs[10][4] = {{0,0,4,0}, {0,0,8,0}, {0,0,16,0}, {0,0,32,0}, 
+					{0,0,64,0}, {0,0,128,0}, {0,1,0,0}, {0,2,0,0}, 
+					{0,4,0,0}, {0,8,0,0}};
 
-int midLEDs[6][4] = {{1,0,0,0}, {2,0,0,0}, {4,0,0,0}, {8,0,0,0},
-                     {16,0,0,0}, {32,0,0,0}};
+int leftMidLEDs[4][4] = {{1,0,0,0}, {2,0,0,0}, {4,0,0,0}, {8,0,0,0}};
+
+int rightMidLEDs[4][4] = {{16,0,0,0}, {32,0,0,0}, {64,0,0,0}, {128,0,0,0}};
 
 void setup() {
+  Serial.begin(9600);
   pinMode(latchPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
@@ -45,27 +48,39 @@ void loop() {
   buttonStateIn = digitalRead(buttonIn);
   buttonStateRail = digitalRead(buttonRail);
   
+  if (currState == "mid") {
+    maxPos = 3;
+  } else {
+    maxPos = 9;
+  }
+  
   if (buttonStateOut == HIGH) {
-    i++;
+    if (i != maxPos) {
+    	i++;
+    } else {
+      	i = 0;
+    }
     delay(150);
-    Serial.print('hi');
   }
   
   if (buttonStateIn == HIGH) {
-    i--;
+    if (i != 0) {
+    	i--;
+    } else {
+      	i = maxPos;
+    }
     delay(150);
-    Serial.print('no');
   }
   
   if (buttonStateRail == HIGH) {
     if (currState == "left") {
-      currState = "mid";
-        prevState = "left";
-        prevPos = i;
+    	currState = "mid";
+      	prevState = "left";
+      	prevPos = i;
     } else if (currState == "right") {
-    currState = "mid";
-        prevState = "right";
-        prevPos = i;
+		currState = "mid";
+      	prevState = "right";
+      	prevPos = i;
     } else if (currState == "mid") {
       if (prevState == "right") {
         currState = "left";
@@ -76,7 +91,6 @@ void loop() {
     i = 0;
     delay(150);
   }
-  
   digitalWrite(latchPin, LOW);
   if (currState == "left") {
     shiftOut(dataPin, clockPin, MSBFIRST, leftLEDs[i][0]);
@@ -90,16 +104,16 @@ void loop() {
     shiftOut(dataPin, clockPin, MSBFIRST, rightLEDs[i][3]);
   } else if (currState == "mid") {
     if (prevState == "left") {
-      shiftOut(dataPin, clockPin, MSBFIRST, midLEDs[i][0] + leftLEDs[prevPos][0]);
-      shiftOut(dataPin, clockPin, MSBFIRST, midLEDs[i][1] + leftLEDs[prevPos][1]);
-      shiftOut(dataPin, clockPin, MSBFIRST, midLEDs[i][2] + leftLEDs[prevPos][2]);
-      shiftOut(dataPin, clockPin, MSBFIRST, midLEDs[i][3] + leftLEDs[prevPos][3]);
+      shiftOut(dataPin, clockPin, MSBFIRST, leftMidLEDs[i][0] + leftLEDs[prevPos][0]);
+      shiftOut(dataPin, clockPin, MSBFIRST, leftMidLEDs[i][1] + leftLEDs[prevPos][1]);
+      shiftOut(dataPin, clockPin, MSBFIRST, leftMidLEDs[i][2] + leftLEDs[prevPos][2]);
+      shiftOut(dataPin, clockPin, MSBFIRST, leftMidLEDs[i][3] + leftLEDs[prevPos][3]);
     } else if (prevState == "right") {
-      j = 5-i;
-      shiftOut(dataPin, clockPin, MSBFIRST, midLEDs[j][0] + rightLEDs[prevPos][0]);
-      shiftOut(dataPin, clockPin, MSBFIRST, midLEDs[j][1] + rightLEDs[prevPos][1]);
-      shiftOut(dataPin, clockPin, MSBFIRST, midLEDs[j][2] + rightLEDs[prevPos][2]);
-      shiftOut(dataPin, clockPin, MSBFIRST, midLEDs[j][3] + rightLEDs[prevPos][3]);
+      j = 3-i;
+      shiftOut(dataPin, clockPin, MSBFIRST, rightMidLEDs[j][0] + rightLEDs[prevPos][0]);
+      shiftOut(dataPin, clockPin, MSBFIRST, rightMidLEDs[j][1] + rightLEDs[prevPos][1]);
+      shiftOut(dataPin, clockPin, MSBFIRST, rightMidLEDs[j][2] + rightLEDs[prevPos][2]);
+      shiftOut(dataPin, clockPin, MSBFIRST, rightMidLEDs[j][3] + rightLEDs[prevPos][3]);
     }
     
   }
