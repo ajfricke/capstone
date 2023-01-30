28501b47481de1710879d8ccf783281852281807
Alex Fricke <ajfricke@edu.uwaterloo.ca>
2023-01-29 22:45:34 -0500

    Reorganize aggregated script

--- main/aggregated_script.ino
+++ main/aggregated_script.ino
@@ -1,7 +1,7 @@
 #include <AccelStepper.h>
 #include <Servo.h>
 
-//** PINS & VARIABLES FOR ALL **//
+//** GENERAL INITIALIZATIONS **//
 Servo myServo;
 int mode = 0; // 0 for waiting, 1 for calibration, 2 for probing
 
@@ -16,7 +16,8 @@ bool newData = false;
 bool recvdAppData = true;
 //** END **//
 
-//** PINS & VARIABLES FOR PROBING **//
+
+//** PROBING INITIALIZATIONS **//
 // define pins
 #define dirPin100 4
 #define stepPin100 5
@@ -36,13 +37,25 @@ bool recvdAppData = true;
 #define strokeStepMM 2
 #define delayMS 100
 
-int leftLEDPos = -1;
-int rightLEDPos = -1;
-int leftMidLEDPos = -1;
-int rightMidLEDPos = -1;
-//** END ** //
+// 1st metatarsal (x,y) coordinates for different foot sizes
+float m1CoordList[9][2] = {{18.52799,163.83845}, {19.41122,171.12136}, {20.0718,177.67446}, 
+                                {21.39691,184.95694}, {21.83603,189.32687}, {22.49935,196.60877}, 
+                                {22.93777,203.16239}, {24.03872,208.2592}, {24.70276,214.08506}};
 
-//** PINS & VARIABLES FOR CALIBRATION **//
+// conversion factors to get the other probing locations
+float m3Conversions[2] = {2.666243932,1.004125611};
+float m5Conversions[2] = {4.166072072,0.962256948};
+float bigToeConversions[2] = {1,1.231986944};
+float newReferencePoint[2] = {55, 181.54159};
+float w5FootsizeCoords[4][2] = {{17.86979, 158.3}, {47.63918, 157.93545}, {74.43552, 151.35002}, {17.86979, 193.75}};
+float probingCoords[4][2];
+
+AccelStepper stepper100 = AccelStepper(motorInterfaceType, stepPin100, dirPin100);
+AccelStepper stepper300 = AccelStepper(motorInterfaceType, stepPin300, dirPin300);
+//** END **//
+
+
+//** CALIBRATION INITIALIZATIONS **//
 #define buttonRail 2
 #define buttonIn 3
 #define buttonOut 4 // TODO: DUPLICATE PIN - change
@@ -62,22 +75,52 @@ int leftMidLEDs[4][4] = {{1,0,0,0}, {2,0,0,0}, {4,0,0,0}, {8,0,0,0}};
 
 int rightMidLEDs[4][4] = {{16,0,0,0}, {32,0,0,0}, {64,0,0,0}, {128,0,0,0}};
 
-// 1st metatarsal (x,y) coordinates for different foot sizes
-float m1CoordList[9][2] = {{18.52799,163.83845}, {19.41122,171.12136}, {20.0718,177.67446}, 
-                                {21.39691,184.95694}, {21.83603,189.32687}, {22.49935,196.60877}, 
-                                {22.93777,203.16239}, {24.03872,208.2592}, {24.70276,214.08506}};
-
-// conversion factors to get the other probing locations
-float m3Conversions[2] = {2.666243932,1.004125611};
-float m5Conversions[2] = {4.166072072,0.962256948};
-float bigToeConversions[2] = {1,1.231986944};
-float newReferencePoint[2] = {55, 181.54159};
-float w5FootsizeCoords[4][2] = {{17.86979, 158.3}, {47.63918, 157.93545}, {74.43552, 151.35002}, {17.86979, 193.75}};
-float probingCoords[4][2];
-
-AccelStepper stepper100 = AccelStepper(motorInterfaceType, stepPin100, dirPin100);
-AccelStepper stepper300 = AccelStepper(motorInterfaceType, stepPin300, dirPin300);
+int leftLEDPos = -1;
+int rightLEDPos = -1;
+int leftMidLEDPos = -1;
+int rightMidLEDPos = -1;
 //** END ** //
+
+
+//** PROBING FUNCTIONS **//
+void getProbingCoords() {
+    // determine the coordinates for each probing location
+    if ((sex == 0) && (footsize == 5)) {
+        for (int k; k < 4; k++) {
+            for (int i; i < 2; i++) {
+                probingCoords[k][i] = w5FootsizeCoords[k][i];
+            }
+        }
+    } else {
+        for (int i; i < 2; i++) {
+            probingCoords[0][i] = m1CoordList[footsize+sex-6][i];
+            probingCoords[1][i] = probingCoords[0][i]*m3Conversions[i];
+            probingCoords[2][i] = probingCoords[0][i]*m5Conversions[i];
+            probingCoords[3][i] = probingCoords[0][i]*bigToeConversions[i];
+        }
+    }
+
+    // recalculate probing coordinates with new reference point
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 2; j++) {
+            probingCoords[i][j] = probingCoords[i][j] - newReferencePoint[j];
+        }
+    }
+
+    // shuffle the sequence of probing locations
+    int j;
+    float temp[2];
+    for (int i = 0; i < 4; i++) {
+        j = random(0, 3 - i); // get random index
+
+        // swap values
+        for (int k; k < 2; k++) {
+            temp[k] = probingCoords[i][k];
+            probingCoords[i][k] = probingCoords[j][k];
+            probingCoords[j][k] = temp[k];
+        }
+    }
+}
 
 
 // convert distances in mm to motor steps
@@ -142,15 +185,74 @@ void resetMotors() {
 }
 
 
-byte getCalibrationInstruc() {
-    if (Serial.available()>0) {
-        byte inputByte = Serial.read();
-        Serial.println('Received data from the app: ');
-        Serial.print(inputByte);
+void probeLoop(String foot) {
+    Serial.println("Starting probing test.");
+    // move rails to each of the 4 probing locations
+    for (int i = 0; i < 4; i++) {
+        Serial.println("Moving XY rails to probing location.");
 
-        return inputByte;
+        // negate x-coordinate if left foot
+        // TODO: should we do this elsewhere? Then wouldn't need to pass in foot. Only used here
+        if (foot == "left") {
+            moveXYRails(-probingCoords[i][0], probingCoords[i][1]);
+        } else {
+            moveXYRails(probingCoords[i][0], probingCoords[i][1]);
+        }
+
+        // test probing location three times
+        int improperProbeCount = 0;
+        for (int j = 0; j < 3; j++) {
+            Serial.println("Raising monofilament.");
+            bool properProbe = raiseMonofilament();
+
+            if (properProbe == false) {
+                j -= 1;
+                improperProbeCount += 1;
+            } else {
+                Serial.println("Input received from the photointerrupters. Holding for 2 seconds");
+                delay(2000); // hold probe to foot for 2 seconds
+            }
+
+            if (improperProbeCount == 3){
+                Serial.println("Monofilament is not exerting 10g force as determined by the photointerrupters after 3 tries. Exiting.");
+                // TODO: tell app we failed
+                resetMotors();
+                return;
+            }
+
+            Serial.println("Lowering monofilament.");
+            moveActuator(0); // lower monofilament back down
+
+            if (j != 2) {
+                delay(random(500, 2000)); // randomize timing between same location probing at intervals between 1-3s
+            }
+        }
+
+        delay(random(500, 2000)); // randomize timing between different location probing at intervals between 1-3s
     }
-    return -1
+
+    Serial.println("Test finished. Resetting motors and exiting.");
+    // TODO: tell app we succeeded
+    resetMotors();
+}
+//** END **//
+
+
+//** CALIBRATION FUNCTIONS **//
+void intializeLEDs(String foot) {
+    digitalWrite(latchPin, LOW);
+    if (foot == "left") {
+        // light up LEDs for left foot
+        for (int i = 0; i < 4; i++) {
+            shiftOut(dataPin, clockPin, MSBFIRST, leftMidLEDs[leftMidLEDPos][i] + leftLEDs[leftLEDPos][i]);
+        }
+    } else if (foot == "right") {
+        // light up LEDs for right foot
+        for (int i = 0; i < 4; i++) {
+            shiftOut(dataPin, clockPin, MSBFIRST, rightMidLEDs[rightLEDPos][i] + rightLEDs[rightLEDPos][i]);
+        }
+    }
+    digitalWrite(latchPin, HIGH);
 }
 
 
@@ -239,138 +341,10 @@ void calibrationLoop(String currState) {
         }
     }  
 }
+//** END **//
 
 
-void probeLoop(String foot) {
-    Serial.println("Starting probing test.");
-    // move rails to each of the 4 probing locations
-    for (int i = 0; i < 4; i++) {
-        Serial.println("Moving XY rails to probing location.");
-
-        // negate x-coordinate if left foot
-        // TODO: should we do this elsewhere? Then wouldn't need to pass in foot. Only used here
-        if (foot == "left") {
-            moveXYRails(-probingCoords[i][0], probingCoords[i][1]);
-        } else {
-            moveXYRails(probingCoords[i][0], probingCoords[i][1]);
-        }
-
-        // test probing location three times
-        int improperProbeCount = 0;
-        for (int j = 0; j < 3; j++) {
-            Serial.println("Raising monofilament.");
-            bool properProbe = raiseMonofilament();
-
-            if (properProbe == false) {
-                j -= 1;
-                improperProbeCount += 1;
-            } else {
-                Serial.println("Input received from the photointerrupters. Holding for 2 seconds");
-                delay(2000); // hold probe to foot for 2 seconds
-            }
-
-            if (improperProbeCount == 3){
-                Serial.println("Monofilament is not exerting 10g force as determined by the photointerrupters after 3 tries. Exiting.");
-                // TODO: tell app we failed
-                resetMotors();
-                return;
-            }
-
-            Serial.println("Lowering monofilament.");
-            moveActuator(0); // lower monofilament back down
-
-            if (j != 2) {
-                delay(random(500, 2000)); // randomize timing between same location probing at intervals between 1-3s
-            }
-        }
-
-        delay(random(500, 2000)); // randomize timing between different location probing at intervals between 1-3s
-    }
-
-    Serial.println("Test finished. Resetting motors and exiting.");
-    // TODO: tell app we succeeded
-    resetMotors();
-}
-
-
-void setup() {
-    Serial.begin(9600);
-    randomSeed(analogRead(5)); // randomize seed using noise from analog pin 5
-
-    // set up actuator
-    myServo.attach(servoPin);
-    myServo.writeMicroseconds(1000);
-
-    // set up stepper motors
-    stepper100.setMaxSpeed(50000);
-    stepper300.setMaxSpeed(50000);
-    stepper100.setAcceleration(50000);
-    stepper300.setAcceleration(50000);
-
-    // set up shift registers
-    pinMode(latchPin, OUTPUT);
-    pinMode(clockPin, OUTPUT);
-    pinMode(dataPin, OUTPUT);
-}
-
-
-void intializeLEDs(String foot) {
-    digitalWrite(latchPin, LOW);
-    if (foot == "left") {
-        // light up LEDs for left foot
-        for (int i = 0; i < 4; i++) {
-            shiftOut(dataPin, clockPin, MSBFIRST, leftMidLEDs[leftMidLEDPos][i] + leftLEDs[leftLEDPos][i]);
-        }
-    } else if (foot == "right") {
-        // light up LEDs for right foot
-        for (int i = 0; i < 4; i++) {
-            shiftOut(dataPin, clockPin, MSBFIRST, rightMidLEDs[rightLEDPos][i] + rightLEDs[rightLEDPos][i]);
-        }
-    }
-    digitalWrite(latchPin, HIGH);
-}
-
-
-void getProbingCoords() {
-    // determine the coordinates for each probing location
-    if ((sex == 0) && (footsize == 5)) {
-        for (int k; k < 4; k++) {
-            for (int i; i < 2; i++) {
-                probingCoords[k][i] = w5FootsizeCoords[k][i];
-            }
-        }
-    } else {
-        for (int i; i < 2; i++) {
-            probingCoords[0][i] = m1CoordList[footsize+sex-6][i];
-            probingCoords[1][i] = probingCoords[0][i]*m3Conversions[i];
-            probingCoords[2][i] = probingCoords[0][i]*m5Conversions[i];
-            probingCoords[3][i] = probingCoords[0][i]*bigToeConversions[i];
-        }
-    }
-
-    // recalculate probing coordinates with new reference point
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 2; j++) {
-            probingCoords[i][j] = probingCoords[i][j] - newReferencePoint[j];
-        }
-    }
-
-    // shuffle the sequence of probing locations
-    int j;
-    float temp[2];
-    for (int i = 0; i < 4; i++) {
-        j = random(0, 3 - i); // get random index
-
-        // swap values
-        for (int k; k < 2; k++) {
-            temp[k] = probingCoords[i][k];
-            probingCoords[i][k] = probingCoords[j][k];
-            probingCoords[j][k] = temp[k];
-        }
-    }
-}
-
-
+//** APP INTERACTION FUNCTIONS **//
 bool getInitialAppInput() {
     bool recvInProgress = false;
     byte ndx = 0;
@@ -433,6 +407,40 @@ bool getInitialAppInput() {
     }
 
     return false;
+}
+
+
+byte getCalibrationInstruc() {
+    if (Serial.available()>0) {
+        byte inputByte = Serial.read();
+        Serial.println('Received data from the app: ');
+        Serial.print(inputByte);
+
+        return inputByte;
+    }
+    return -1
+}
+//** END **//
+
+
+void setup() {
+    Serial.begin(9600);
+    randomSeed(analogRead(5)); // randomize seed using noise from analog pin 5
+
+    // set up actuator
+    myServo.attach(servoPin);
+    myServo.writeMicroseconds(1000);
+
+    // set up stepper motors
+    stepper100.setMaxSpeed(50000);
+    stepper300.setMaxSpeed(50000);
+    stepper100.setAcceleration(50000);
+    stepper300.setAcceleration(50000);
+
+    // set up shift registers
+    pinMode(latchPin, OUTPUT);
+    pinMode(clockPin, OUTPUT);
+    pinMode(dataPin, OUTPUT);
 }
 
 
